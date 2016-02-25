09cffdd31f4ce349e6b275bb4a47adad4d59624a
danielpgross <daniel.gross.2@gmail.com>
2016-02-25 15:20:42 -0500

    PT-1951 Patient measurements - UI improvements fix regression introduced in 7f11be1d1ad5ed795a8c29577e9ab361b54a3df1

--- components/patient-measurements/resources/src/main/resources/resources/uicomponents/phenotips/measurements.js
+++ components/patient-measurements/resources/src/main/resources/resources/uicomponents/phenotips/measurements.js
@@ -806,6 +806,12 @@ var PhenoTips = (function(PhenoTips) {
         return existingElement;
       }
     },
+
+    destroy: function($super) {
+      this._selectAssocPhenotypes([]);
+
+      $super();
+    }
   });
 
   widgets.ArmspanMeasurementSetRow = Class.create(widgets.MeasurementSetRow, {
