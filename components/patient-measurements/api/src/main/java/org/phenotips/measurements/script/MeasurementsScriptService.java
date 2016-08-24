Gene42/phenotips-niaid

PT-1951 Patient measurements - UI improvements PT-2015 make patient age accessible from script service, add fuzzy value to percentile endpoint

components/patient-measurements/api/src/main/java/org/phenotips/measurements/script/MeasurementsScriptService.java
@@ -51,21 +51,6 @@ import org.slf4j.Logger;
 @Singleton
 public class MeasurementsScriptService implements ScriptService
 {
-    /** Fuzzy value representing a measurement value considered extremely below normal. */
-    private static final String VALUE_EXTREME_BELOW_NORMAL = "extreme-below-normal";
-
-    /** Fuzzy value representing a measurement value considered below normal, but not extremely. */
-    private static final String VALUE_BELOW_NORMAL = "below-normal";
-
-    /** Fuzzy value representing a measurement value considered normal. */
-    private static final String VALUE_NORMAL = "normal";
-
-    /** Fuzzy value representing a measurement value considered above normal, but not extremely. */
-    private static final String VALUE_ABOVE_NORMAL = "above-normal";
-
-    /** Fuzzy value representing a measurement value considered extremely above normal. */
-    private static final String VALUE_EXTREME_ABOVE_NORMAL = "extreme-above-normal";
-
     /** Logging helper object. */
     @Inject
     private Logger logger;
@@ -142,17 +127,7 @@ public class MeasurementsScriptService implements ScriptService
      */
     public String getFuzzyValue(int percentile)
     {
-        String returnValue = VALUE_NORMAL;
-        if (percentile <= 1) {
-            returnValue = VALUE_EXTREME_BELOW_NORMAL;
-        } else if (percentile <= 3) {
-            returnValue = VALUE_BELOW_NORMAL;
-        } else if (percentile >= 99) {
-            returnValue = VALUE_EXTREME_ABOVE_NORMAL;
-        } else if (percentile >= 97) {
-            returnValue = VALUE_ABOVE_NORMAL;
-        }
-        return returnValue;
+        return AbstractMeasurementHandler.getFuzzyValue(percentile);
     }
 
     /**
@@ -163,17 +138,19 @@ public class MeasurementsScriptService implements ScriptService
      */
     public String getFuzzyValue(double deviation)
     {
-        String returnValue = VALUE_NORMAL;
-        if (deviation <= -3.0) {
-            returnValue = VALUE_EXTREME_BELOW_NORMAL;
-        } else if (deviation <= -2.0) {
-            returnValue = VALUE_BELOW_NORMAL;
-        } else if (deviation >= 3.0) {
-            returnValue = VALUE_EXTREME_ABOVE_NORMAL;
-        } else if (deviation >= 2.0) {
-            returnValue = VALUE_ABOVE_NORMAL;
-        }
-        return returnValue;
+        return AbstractMeasurementHandler.getFuzzyValue(deviation);
+    }
+
+    /**
+     * Get the number of months corresponding to a period string.
+     *
+     * @param age the ISO-8601 period string, without leading 'P'
+     * @throws IllegalArgumentException if the age cannot be parsed
+     * @return number of months
+     */
+    public Double convertAgeStrToNumMonths(String age) throws IllegalArgumentException
+    {
+        return AbstractMeasurementHandler.convertAgeStrToNumMonths(age);
     }
 
     /**

components/patient-measurements/rest/src/main/java/org/phenotips/data/rest/internal/DefaultMeasurementChartResourcesResourceImpl.java
@@ -20,6 +20,7 @@ package org.phenotips.data.rest.internal;
 import org.phenotips.data.rest.MeasurementChartResourcesResource;
 import org.phenotips.measurements.MeasurementHandler;
 import org.phenotips.measurements.MeasurementsChartConfiguration;
+import org.phenotips.measurements.internal.AbstractMeasurementHandler;
 
 import org.xwiki.bridge.DocumentAccessBridge;
 import org.xwiki.component.annotation.Component;
@@ -87,7 +88,7 @@ public class DefaultMeasurementChartResourcesResourceImpl extends AbstractMeasur
                     JSONObject measurements = measurementSet.getJSONObject("measurements");
                     double ageMonths;
                     try {
-                        ageMonths = AbstractMeasurementRestResource.convertAgeStrToNumMonths(age);
+                        ageMonths = AbstractMeasurementHandler.convertAgeStrToNumMonths(age);
                     } catch (IllegalArgumentException e) {
                         throw new WebApplicationException(generateErrorResponse(Response.Status.BAD_REQUEST,
                                 "Cannot parse age."));

components/patient-measurements/rest/src/main/java/org/phenotips/data/rest/internal/DefaultMeasurementPercentileResourceImpl.java
@@ -19,6 +19,7 @@ package org.phenotips.data.rest.internal;
 
 import org.phenotips.data.rest.MeasurementPercentileResource;
 import org.phenotips.measurements.MeasurementHandler;
+import org.phenotips.measurements.internal.AbstractMeasurementHandler;
 
 import org.xwiki.component.annotation.Component;
 
@@ -53,7 +54,7 @@ public class DefaultMeasurementPercentileResourceImpl extends AbstractMeasuremen
 
         Double ageMonths;
         try {
-            ageMonths = AbstractMeasurementRestResource.convertAgeStrToNumMonths(age);
+            ageMonths = AbstractMeasurementHandler.convertAgeStrToNumMonths(age);
         } catch (IllegalArgumentException e) {
             throw new WebApplicationException(generateErrorResponse(Response.Status.BAD_REQUEST, "Cannot parse age."));
         }
@@ -67,7 +68,9 @@ public class DefaultMeasurementPercentileResourceImpl extends AbstractMeasuremen
 
         JSONObject resp = new JSONObject();
         resp.accumulate("percentile", handler.valueToPercentile(isMale, ageMonths.floatValue(), value));
-        resp.accumulate("stddev", handler.valueToStandardDeviation(isMale, ageMonths.floatValue(), value));
+        double stddev = handler.valueToStandardDeviation(isMale, ageMonths.floatValue(), value);
+        resp.accumulate("stddev", stddev);
+        resp.accumulate("fuzzy-value", AbstractMeasurementHandler.getFuzzyValue(stddev));
 
         return Response.ok(resp, MediaType.APPLICATION_JSON_TYPE).build();
     }

sdumitriu/phenotips-1

components/patient-measurements/api/src/main/java/org/phenotips/measurements/script/MeasurementsScriptService.java
@@ -19,6 +19,7 @@ package org.phenotips.measurements.script;
 
 import org.phenotips.measurements.MeasurementHandler;
 import org.phenotips.measurements.internal.AbstractMeasurementHandler;
+import org.phenotips.measurements.internal.MeasurementUtils;
 
 import org.xwiki.component.annotation.Component;
 import org.xwiki.component.manager.ComponentLookupException;
@@ -51,21 +52,6 @@ import org.slf4j.Logger;
 @Singleton
 public class MeasurementsScriptService implements ScriptService
 {
-    /** Fuzzy value representing a measurement value considered extremely below normal. */
-    private static final String VALUE_EXTREME_BELOW_NORMAL = "extreme-below-normal";
-
-    /** Fuzzy value representing a measurement value considered below normal, but not extremely. */
-    private static final String VALUE_BELOW_NORMAL = "below-normal";
-
-    /** Fuzzy value representing a measurement value considered normal. */
-    private static final String VALUE_NORMAL = "normal";
-
-    /** Fuzzy value representing a measurement value considered above normal, but not extremely. */
-    private static final String VALUE_ABOVE_NORMAL = "above-normal";
-
-    /** Fuzzy value representing a measurement value considered extremely above normal. */
-    private static final String VALUE_EXTREME_ABOVE_NORMAL = "extreme-above-normal";
-
     /** Logging helper object. */
     @Inject
     private Logger logger;
@@ -142,17 +128,7 @@ public class MeasurementsScriptService implements ScriptService
      */
     public String getFuzzyValue(int percentile)
     {
-        String returnValue = VALUE_NORMAL;
-        if (percentile <= 1) {
-            returnValue = VALUE_EXTREME_BELOW_NORMAL;
-        } else if (percentile <= 3) {
-            returnValue = VALUE_BELOW_NORMAL;
-        } else if (percentile >= 99) {
-            returnValue = VALUE_EXTREME_ABOVE_NORMAL;
-        } else if (percentile >= 97) {
-            returnValue = VALUE_ABOVE_NORMAL;
-        }
-        return returnValue;
+        return MeasurementUtils.getFuzzyValue(percentile);
     }
 
     /**
@@ -163,17 +139,19 @@ public class MeasurementsScriptService implements ScriptService
      */
     public String getFuzzyValue(double deviation)
     {
-        String returnValue = VALUE_NORMAL;
-        if (deviation <= -3.0) {
-            returnValue = VALUE_EXTREME_BELOW_NORMAL;
-        } else if (deviation <= -2.0) {
-            returnValue = VALUE_BELOW_NORMAL;
-        } else if (deviation >= 3.0) {
-            returnValue = VALUE_EXTREME_ABOVE_NORMAL;
-        } else if (deviation >= 2.0) {
-            returnValue = VALUE_ABOVE_NORMAL;
-        }
-        return returnValue;
+        return MeasurementUtils.getFuzzyValue(deviation);
+    }
+
+    /**
+     * Get the number of months corresponding to a period string.
+     *
+     * @param age the ISO-8601 period string, without leading 'P'
+     * @throws IllegalArgumentException if the age cannot be parsed
+     * @return number of months
+     */
+    public Double convertAgeStrToNumMonths(String age) throws IllegalArgumentException
+    {
+        return MeasurementUtils.convertAgeStrToNumMonths(age);
     }
 
     /**

components/patient-measurements/rest/src/main/java/org/phenotips/data/rest/internal/DefaultMeasurementChartResourcesResourceImpl.java
@@ -20,6 +20,7 @@ package org.phenotips.data.rest.internal;
 import org.phenotips.data.rest.MeasurementChartResourcesResource;
 import org.phenotips.measurements.MeasurementHandler;
 import org.phenotips.measurements.MeasurementsChartConfiguration;
+import org.phenotips.measurements.internal.MeasurementUtils;
 
 import org.xwiki.bridge.DocumentAccessBridge;
 import org.xwiki.component.annotation.Component;
@@ -87,7 +88,7 @@ public class DefaultMeasurementChartResourcesResourceImpl extends AbstractMeasur
                     JSONObject measurements = measurementSet.optJSONObject("measurements");
                     double ageMonths;
                     try {
-                        ageMonths = AbstractMeasurementRestResource.convertAgeStrToNumMonths(age);
+                        ageMonths = MeasurementUtils.convertAgeStrToNumMonths(age);
                     } catch (IllegalArgumentException e) {
                         throw new WebApplicationException(generateErrorResponse(Response.Status.BAD_REQUEST,
                             "Cannot parse age."));

components/patient-measurements/rest/src/main/java/org/phenotips/data/rest/internal/DefaultMeasurementPercentileResourceImpl.java
@@ -19,6 +19,7 @@ package org.phenotips.data.rest.internal;
 
 import org.phenotips.data.rest.MeasurementPercentileResource;
 import org.phenotips.measurements.MeasurementHandler;
+import org.phenotips.measurements.internal.MeasurementUtils;
 
 import org.xwiki.component.annotation.Component;
 
@@ -53,7 +54,7 @@ public class DefaultMeasurementPercentileResourceImpl extends AbstractMeasuremen
 
         Double ageMonths;
         try {
-            ageMonths = AbstractMeasurementRestResource.convertAgeStrToNumMonths(age);
+            ageMonths = MeasurementUtils.convertAgeStrToNumMonths(age);
         } catch (IllegalArgumentException e) {
             throw new WebApplicationException(generateErrorResponse(Response.Status.BAD_REQUEST, "Cannot parse age."));
         }
@@ -67,7 +68,9 @@ public class DefaultMeasurementPercentileResourceImpl extends AbstractMeasuremen
 
         JSONObject resp = new JSONObject();
         resp.accumulate("percentile", handler.valueToPercentile(isMale, ageMonths.floatValue(), value));
-        resp.accumulate("stddev", handler.valueToStandardDeviation(isMale, ageMonths.floatValue(), value));
+        double stddev = handler.valueToStandardDeviation(isMale, ageMonths.floatValue(), value);
+        resp.accumulate("stddev", stddev);
+        resp.accumulate("fuzzy-value", MeasurementUtils.getFuzzyValue(stddev));
 
         return Response.ok(resp, MediaType.APPLICATION_JSON_TYPE).build();
     }